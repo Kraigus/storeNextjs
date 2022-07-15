2384118fbac2b545c9c5cda74521d9068f21bf41
Елисеев Антон Николаевич <ANEliseev@sberbank.ru>
2022-07-15 10:13:21 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Htag, Button } from "../components";
+
+export default function Home(): JSX.Element {
+	return (
+		<>
+			<Htag tag="h1">Текст</Htag>
+			<Htag tag="h2">Текст</Htag>
+			<Htag tag="h3">Текст</Htag>
+			<Button appearance="primary" className="sdfsf">KHOIIKA</Button>
+			<Button appearance="ghost">KHOIIKA</Button>
+		</>
+	);
+}
